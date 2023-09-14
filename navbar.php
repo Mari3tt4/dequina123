e22539d67ffaa1204b83685a4055dff3f18810e0
Marinelle Klyne Dequina <marinelleklynedequina@gmail.com>
2023-09-14 14:04:33 +0800

    first commit

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+    <nav class="list-none flex items- center inline-block font-Times New Roman font-bold">
+        <li class="m-5"><a href="index.php" class="no-underline text-white px2">Home</a></li>
+        <li class="m-5"><a href="index.php" class="no-underline text-white px2">About</a></li>
+        <li class="m-5"><a href="index.php" class="no-underline text-white px2">Contact</a></li>
+</nav>
+</div>
+</body>
+</html> 
